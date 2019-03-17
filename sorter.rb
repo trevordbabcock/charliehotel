b8c584e4a06a460229a282a0d271b3b8b4e35ae6
Trevor Babcock <trevor.d.babcock@gmail.com>
2019-03-16 19:33:40 -0700

    First pass at implementing CSV sorter.

--- sorter.rb
+++ sorter.rb
@@ -1,1 +1,31 @@
-puts "test"
+module TDB
+  class CSV
+    attr_accessor :values
+
+    def initialize(csv_string)
+      # TODO validate csv?
+      self.values = csv_to_array(csv_string)
+    end
+
+    def sort!
+      self.values.sort!
+    end
+
+    def to_s
+      self.values.join(",")
+    end
+
+    protected
+
+    def csv_to_array(csv_string)
+      csv_string.split(",")
+    end
+  end
+end
+
+csv_string = "Copenhagen,Stockholm,Oslo"
+
+csv = TDB::CSV.new(csv_string)
+csv.sort!
+
+puts csv.to_s
